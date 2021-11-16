d4cdfbb3d947762ffae83af1cabcdd3f597ce7c1
fah <fah@ciklum.com>
2021-11-16 16:38:49 +0500

    Architecture for Vue application

--- src/views/authorized/Dashboard.vue
+++ src/views/authorized/Dashboard.vue
@@ -1,23 +1,104 @@
 <template>
-    <h1>Welcome to Dashboard</h1>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" @click="logoutHanlder">
-        Button
+  <title-bar :title-stack="titleStack" />
+  <hero-bar>Dashboard</hero-bar>
+  <main-section>
+    <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 mb-6">
+      <card-widget
+        trend="12%"
+        trend-type="up"
+        color="text-green-500"
+        :icon="mdiAccountMultiple"
+        :number="512"
+        label="Clients"
+      />
+      <card-widget
+        trend="12%"
+        trend-type="down"
+        color="text-blue-500"
+        :icon="mdiCartOutline"
+        :number="7770"
+        prefix="$"
+        label="Sales"
+      />
+      <card-widget
+        trend="Overflow"
+        trend-type="alert"
+        color="text-red-500"
+        :icon="mdiChartTimelineVariant"
+        :number="256"
+        suffix="%"
+        label="Performance"
+      />
+    </div>
+    <button
+      class="
+        bg-blue-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded-full
+      "
+      @click="logoutHanlder"
+    >
+      Button
     </button>
-
+  </main-section>
 </template>
 <script>
-import utility from '../../components/composition/utility';
-export default {
-    setup() {
-      const logoutHanlder = async ()=>{
-           await localStorage.removeItem("authToken")
-           const { navigateTo } = utility('login')
-           navigateTo()
-       }
+import { computed, ref, onMounted } from 'vue'
+import utility from "../../components/composition/utility";
+import MainSection from "@/components/MainSection";
+import TitleBar from "@/components/TitleBar";
+import HeroBar from "@/components/HeroBar";
+import CardWidget from "@/components/CardWidget";
+import CardComponent from "@/components/CardComponent";
+import TitleSubBar from "@/components/TitleSubBar";
+import JbButton from "@/components/JbButton";
+import {
+  mdiAccountMultiple,
+  mdiCartOutline,
+  mdiChartTimelineVariant,
+  mdiFinance,
+  mdiMonitorCellphone,
+  mdiReload,
+  mdiGithub,
+  mdiChartPie,
+} from "@mdi/js";
 
-       return {
-           logoutHanlder
-       }
-    },
-}
+export default {
+  name: "Dashboard",
+  components: {
+    TitleSubBar,
+    MainSection,
+    CardWidget,
+    HeroBar,
+    TitleBar,
+    Notification,
+    JbButton,
+  },
+  setup() {
+    const logoutHanlder = async () => {
+      await localStorage.removeItem("authToken");
+      const { navigateTo } = utility("login");
+      navigateTo();
+    };
+
+    const titleStack = ref(["Admin", "Dashboard"]);
+    const darkMode = computed(() => store.state.darkMode);
+    return {
+      logoutHanlder,
+      darkMode,
+      mdiAccountMultiple,
+      mdiCartOutline,
+      mdiChartTimelineVariant,
+      mdiFinance,
+      mdiMonitorCellphone,
+      mdiReload,
+      mdiGithub,
+      mdiChartPie,
+    };
+  },
+};
 </script>
