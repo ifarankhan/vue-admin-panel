bf45bcb01e6673b3a44230b2518021865972d1a5
Muhammad Aqib <maq@ciklum.com>
2021-11-26 15:38:40 +0500

    updating login fields color

--- src/views/visitors/Login.vue
+++ src/views/visitors/Login.vue
@@ -18,13 +18,15 @@
         <label class="block mb-1 ml-6">{{ $t("Email") }}</label>
         <div>
           <div class="relative">
-            <input
-              name="login"
-              autocomplete="username"
-              type="text"
-              v-model="form.userName"
-              class="w-11/12 h-12 max-w-full px-3 py-2 ml-3 text-xs bg-white bg-opacity-50 border-gray-700 rounded-full opacity-50 focus:ring-transparent dark:placeholder-gray-400 dark:bg-gray-800 hover:border-psytechBlue"
-            />
+            <div class="relative">
+              <input
+                name="login"
+                autocomplete="username"
+                type="text"
+                v-model="form.userName"
+                class="w-11/12 h-12 max-w-full px-3 py-2 ml-3 text-xs border-gray-700 rounded-full opacity-50  focus:ring-transparent dark:placeholder-gray-400 dark:bg-gray-800"
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -37,7 +39,7 @@
             autocomplete="current-password"
             type="password"
             v-model="form.password"
-            class="w-11/12 h-12 max-w-full px-3 py-2 ml-3 text-xs bg-white bg-opacity-50 border-gray-700 rounded-full opacity-50 focus:ring-transparent dark:placeholder-gray-400 dark:bg-gray-800 hover:border-psytechBlue"
+            class="w-11/12 h-12 max-w-full px-3 py-2 ml-3 text-xs border-gray-700 rounded-full opacity-50  focus:ring-transparent dark:placeholder-gray-400 dark:bg-gray-800"
           />
         </div>
       </div>
@@ -71,7 +73,7 @@
 
       <div class="flex flex-wrap items-center justify-start">
         <button
-          class="inline-flex items-center justify-center w-11/12 p-3 mb-4 ml-3 mr-3 text-base font-semibold text-white transition duration-150 duration-200 border border-blue-600 border-none rounded-full cursor-pointer focus:outline-none whitespace-nowrap focus:ring ring-blue-700 hover:bg-blue-600 bg-psytechBlueDark last:mr-0 hover:bg-psytechWhite hover:text-psytechBlue btn-login"
+          class="inline-flex items-center justify-center w-11/12 p-3 mb-4 ml-3 mr-3 text-base font-semibold text-white transition duration-150 duration-200 border border-blue-600 border-none rounded-full cursor-pointer  focus:outline-none whitespace-nowrap focus:ring ring-blue-700 hover:bg-blue-600 bg-psytechBlueDark last:mr-0 hover:bg-psytechWhite hover:text-psytechBlue btn-login"
           type="submit"
         >
           <span class="px-2">{{ $t("Submit") }}</span>
@@ -80,30 +82,43 @@
 
       <div class="flex items-center justify-center">
         <div>
-          <language-switcher v-slot="{data, toggleLngMenu, languageMenu, setLocale, Language}">
+          <language-switcher
+            v-slot="{ data, toggleLngMenu, languageMenu, setLocale, Language }"
+          >
             <div class="relative inline-block text-xs dropdown">
-                <div class="inline-flex items-center py-1 font-semibold text-gray-700">
-                  <span class="text-xs font-thin"> {{ $t("Language") }}: </span>
-                  <span class="ml-1 text-xs font-bold" @click.prevent="toggleLngMenu">{{
-                    $t(Language)
-                  }}</span>
-                  <span class="block ml-0.5 mt-0.5">
-                    <LanguageIcon  @click.prevent="toggleLngMenu" />
-                  </span>
-                </div>
-                <ul v-if="languageMenu" :class="Language == 'Arabic' ? 'menu-position-ar':'menu-position'" id="style-2">
-                  <li
-                    class=""
-                    v-for="item in data"
-                    :key="item.value"
-                    @click.prevent="setLocale(item.value)"
-                  >
-                    <a class="block px-4 py-2 whitespace-no-wrap hover:bg-gray-200">{{
-                      item.text
-                    }}</a>
-                  </li>
-                </ul>
+              <div
+                class="inline-flex items-center py-1 font-semibold text-gray-700 "
+              >
+                <span class="text-xs font-thin"> {{ $t("Language") }}: </span>
+                <span
+                  class="ml-1 text-xs font-bold"
+                  @click.prevent="toggleLngMenu"
+                  >{{ $t(Language) }}</span
+                >
+                <span class="block ml-0.5 mt-0.5">
+                  <LanguageIcon @click.prevent="toggleLngMenu" />
+                </span>
               </div>
+              <ul
+                v-if="languageMenu"
+                :class="
+                  Language == 'Arabic' ? 'menu-position-ar' : 'menu-position'
+                "
+                id="style-2"
+              >
+                <li
+                  class=""
+                  v-for="item in data"
+                  :key="item.value"
+                  @click.prevent="setLocale(item.value)"
+                >
+                  <a
+                    class="block px-4 py-2 whitespace-no-wrap hover:bg-gray-200"
+                    >{{ item.text }}</a
+                  >
+                </li>
+              </ul>
+            </div>
           </language-switcher>
         </div>
       </div>
@@ -183,7 +198,7 @@ export default {
         })
         .then(async (res) => {
           let info = res?.data?.data;
-          
+
           if (info.token) {
             const USER_DATA = {
               authToken: info?.token,
@@ -226,7 +241,7 @@ export default {
 .error-class input {
   border-color: #ff0202cf !important;
 }
-.hover {
+.hover label {
   color: #a2abab;
 }
 .hover input {
@@ -243,7 +258,7 @@ export default {
 .dropdown:hover .dropdown-menu {
   display: block;
 }
-.menu-position{
+.menu-position {
   position: absolute;
   top: -64px;
   left: 122px;
@@ -251,7 +266,7 @@ export default {
   overflow: auto;
 }
 
-.menu-position-ar{
+.menu-position-ar {
   position: absolute;
   top: -58px;
   left: 77px;
