9b6442a9acf92deb1e7bd4e6f7c52778f894a5f3
Muhammad Aqib <maq@ciklum.com>
2021-12-16 12:57:47 +0500

    Account Users

--- src/views/authorized/client-control/ClientDetail.vue
+++ src/views/authorized/client-control/ClientDetail.vue
@@ -1,14 +1,14 @@
 <template>
   <sticky-header>
     <div class="grid grid-cols-2 md:px-2">
-      <div class="flex items-center pl-4 ml-4">
+      <div class="flex items-center ml-8">
         <div
-          class="flex items-center justify-center text-white bg-black rounded rounded-full cursor-pointer w-7 h-7 "
+          class="flex items-center justify-center w-8 h-8 text-white bg-black rounded rounded-full cursor-pointer "
           @click="$router.push({ name: 'list-page' })"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="w-4 h-4"
+            class="w-6 h-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -21,7 +21,7 @@
             />
           </svg>
         </div>
-        <div class="ml-3 font-bold truncate text-medium">{{ accountDetail && accountDetail.accountName }} </div>
+        <div class="ml-3 font-bold text-medium">{{ accountDetail && accountDetail.accountName }} </div>
       </div>
       <div class="mr-11 place-self-end">
         <span class="text-sm font-semibold"> Creation Date: </span>
@@ -309,7 +309,233 @@
             </div>
           </div>
         </TabPanel>
-        
+
+        <TabPanel>
+            <div class="mt-4 ml-5">
+          <psytech-button
+            @buttonWasClicked="
+              $router.push({ name: 'client-control-add-user' })
+            "
+            label="Add User"
+            type="outline"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="w-5 h-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </psytech-button>
+          <!-- Create New Client Account -->
+        </div>
+        <!-- paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown" -->
+        <div class="mt-4 mb-2 ml-5">
+          <p class="pl-2 text-sm font-semibold">Master User:</p>
+          <div class="mt-2 mr-10 md:pr-12 lg:pr-0">
+             <DataTable
+                :customers="customers"
+                :rowHover="true"
+                :loading="loading"
+                @rowClicked="''"
+                :image='true'
+              />
+          </div>
+        </div>
+
+        <!--    -->
+        <div class="grid mt-5 main-grid md:grid-cols-2">
+        <!-- left section -->
+        <div class="flex items-end mb-5 ml-4">
+         <p class="pl-2 text-sm font-semibold">Master User:</p>
+        </div>
+
+        <!-- Right Section -->
+        <div class="flex justify-end mb-5 mr-10 xs:flex-col">
+          <div>
+            <div class="relative inline-block dropdown">
+              <psytech-button
+                label="Filter"
+                type="outline"
+                @buttonWasClicked="showFilters = !showFilters"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
+                  />
+                </svg>
+              </psytech-button>
+              <ul
+                class="absolute w-24 text-gray-700 bg-white rounded-md shadow top-14 -left-16 dropdown-menu"
+                style="padding: 16px 15px"
+                id="filter-dropdown"
+                v-if="showFilters"
+                v-click-away="closeFilter"
+              >
+                <li class="flex">
+                  <field
+                    label="Name"
+                    labelFor="account"
+                    :applyExtraInputClass="true"
+                  >
+                    <control
+                      v-model="accountName"
+                      type="text"
+                      id="name"
+                      placeholder=" "
+                    />
+                  </field>
+                  <select-option
+                    :filterDropdown="filterDropdown"
+                    v-model="selectedNameFilter"
+                  ></select-option>
+                  <!-- <field label="Filter Type" labelFor="filterType" :applyExtraSelectClass="true">
+                    <control :options="filterDropdown" type="select" v-model="selectedNameFilter" />
+                  </field> -->
+                  <div class="flex items-center justify-center mt-1">
+                    <IconSVG
+                      @iconWasClicked="(accountName = ''), applyFilter()"
+                    />
+                  </div>
+                </li>
+
+                <li class="flex">
+                  <field
+                    label="Address"
+                    labelFor="address"
+                    :applyExtraInputClass="true"
+                  >
+                    <control
+                      v-model="searchedaddress"
+                      type="text"
+                      id="address"
+                      placeholder=" "
+                    />
+                  </field>
+
+                  <select-option
+                    :filterDropdown="filterDropdown"
+                    v-model="selectedaddressFilter"
+                  ></select-option>
+
+                  <!-- <field label="Filter Type" labelFor="filterType" :applyExtraSelectClass="true">
+                    <control :options="filterDropdown" type="select" v-model="selectedaddressFilter" />
+                  </field> -->
+                  <div class="flex items-center justify-center mt-1">
+                    <IconSVG
+                      @iconWasClicked="(searchedaddress = ''), applyFilter()"
+                    />
+                  </div>
+                </li>
+
+                <li class="flex">
+                  <field
+                    label="Users"
+                    labelFor="users"
+                    :applyExtraInputClass="true"
+                  >
+                    <control
+                      v-model="searchedUsers"
+                      type="text"
+                      id="users"
+                      placeholder=" "
+                    />
+                  </field>
+
+                  <select-option
+                    :filterDropdown="numberDropdown"
+                    v-model="selectedUsersFilter"
+                  ></select-option>
+
+                  <!-- <field label="Filter Type" labelFor="filterType" :applyExtraSelectClass="true">
+                    <control :options="filterDropdown" type="select" v-model="selectedUsersFilter" />
+                  </field> -->
+                  <div class="flex items-center justify-center mt-1">
+                    <IconSVG
+                      @iconWasClicked="(searchedUsers = ''), applyFilter()"
+                    />
+                  </div>
+                </li>
+                <li class="flex justify-between">
+                  <div class="flex items-center justify-center ml-3">
+                    <a
+                      @click.prevent="clearFilter()"
+                      class="text-xs font-semibold no-underline text-psytechBlue"
+                      >Clear all</a
+                    >
+                  </div>
+                  <psytech-button
+                    label="Apply"
+                    @buttonWasClicked="applyFilter"
+                  ></psytech-button>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <!-- search -->
+          <div class="relative pt-2 text-gray-600">
+            <input
+              class="h-10 px-5 pr-16 font-bold text-gray-700 bg-white border-gray-300 rounded-full border-1 focus:outline-none focus:ring-psytechBlueBtHover"
+              type="search"
+              name="search"
+              placeholder="Search..."
+              v-model="searchText"
+              @input="filteredMainMethod()"
+            />
+            <button type="submit" class="absolute top-0 right-0 mt-5 mr-4">
+              <svg
+                class="w-4 h-4 text-gray-600 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 56.966 56.966"
+                style="enable-background: new 0 0 56.966 56.966"
+                xml:space="preserve"
+                width="512px"
+                height="512px"
+              >
+                <path
+                  d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
+                />
+              </svg>
+            </button>
+          </div>
+          <!-- end search -->
+        </div>
+      </div>
+      <!-- table two -->
+       <div class="mt-2 ml-5">
+          <div class="mr-10 md:pr-12 lg:pr-0">
+             <DataTable
+                :customers="customersTwo"
+                :rowHover="true"
+                :loading="loading"
+                @rowClicked="''"
+                :image='true'
+              />
+          </div>
+        </div>
+        </TabPanel>
       </TabGroup>
     </div>
   </sticky-header>
@@ -442,9 +668,5 @@ export default {
 .div-hover:hover svg line {
   stroke: #008ac0;
   color: #008ac0;
-}
-.truncate > div {
-    white-space:pre-wrap;
-    word-wrap:break-word;
 }
 </style>
