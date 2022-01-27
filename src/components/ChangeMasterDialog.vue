c0765ab74c65c50d9f8cb3f56e18a57d621853db
fah <fah@ciklum.com>
2022-01-27 12:30:58 +0500

    Change master user dialog

--- src/components/ChangeMasterDialog.vue
+++ src/components/ChangeMasterDialog.vue
@@ -4,6 +4,13 @@
           <template #header>
               <h3 class="text-lg font-medium">{{ topHeaderText }}</h3>
             </template>
+            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" v-if="showMsg">
+              <strong class="font-bold">Error!</strong>
+              <span class="block sm:inline">{{showMsg}}</span>
+              <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+              </span>
+            </div>
             <p class="text-sm font-semibold text-black w-80">Please select a Master User</p>
             <div class="flex mt-1">
                 <select-option
@@ -89,6 +96,7 @@ export default {
     },
     setup(props, { emit }) {
        const showDialog = ref(true);
+       const showMsg = ref();
        const userArray = ref();
        const masterArray = ref();
       const form = reactive({
@@ -132,11 +140,14 @@ export default {
                if(responseArray.updatedMasterUser){
                  closeDialog();
                  emit('refreshUserList');
+               }else {
+                 showMsg.value = "There is some issue with the API";
                }
 
              })
              .catch((error) => {
                console.log("error is...", error);
+               showMsg.value = error;
              }).finally(()=>{
               loading.value = false;
          })
@@ -163,6 +174,7 @@ export default {
         closeDialog,
         changeMaster,
         v$,
+        showMsg
       }
     },
 }
