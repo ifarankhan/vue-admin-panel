12ab7cd2b3ebf5aa8c62196e27964e5af0bad5e7
fah <fah@ciklum.com>
2022-04-18 20:50:53 +0500

    Change password process

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -1,6 +1,7 @@
 export const conf = window._env_ || process.env;
 export const AppConfig = {
     BASE_URL: conf.VUE_APP_BASE_URL,
+    BASE_URL_CHANGEPASS: conf.VUE_APP_BASE_URL_CHANGEPASS,
     FRESH_DESK_URL:  conf.VUE_APP_FRESH_DESK_URL
 }
 export default AppConfig;
