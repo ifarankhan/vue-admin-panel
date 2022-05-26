d3dc92edeb13cf531f15cb76192dd8c8070ea95d
fah <fah@ciklum.com>
2022-05-26 13:17:01 +0500

    Merge branch 'develop' of https://github.com/psytech-international-ltd/distributorPanel into PP-273
    # Conflicts:
    #	src/views/authorized/financial-control/deductions/DistributorAdd.vue

--- src/views/authorized/financial-control/deductions/DistributorAdd.vue
+++ src/views/authorized/financial-control/deductions/DistributorAdd.vue
@@ -5,12 +5,13 @@
     </sticky-header>
     <main-section class="grid grid-cols-3 gap-4">
       <div class="col-span-3 form">
-        <error-alert
-            v-if="form.error"
-            @dismissError="form.error = ''"
-            :error="form.error"
-            :showTranslatedError="false"
-        />
+        <div class="relative px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded" role="alert" v-if="showMsg">
+          <strong class="font-bold">Error!</strong>
+          <span class="block sm:inline">{{showMsg}}</span>
+          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="w-6 h-6 text-red-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+              </span>
+        </div>
         <div
             v-if="showSuccessAlert"
             class="flex items-center justify-center px-2 py-1 m-1 font-medium text-green-700 bg-white bg-green-100 border border-green-300 rounded-md "
@@ -175,7 +176,7 @@ export default {
   },
   setup () {
     const form = reactive({
-      totalCost: 0,
+      totalCost:"",
       itemType: "",
       quantity: "",
       date: "",
@@ -183,6 +184,7 @@ export default {
       itemDescription: "",
       loader:false,
     });
+    const showMsg = ref();
     const store = useStore()
     const showSuccessAlert = ref(false);
     const itemTypeArray =[
@@ -299,7 +301,8 @@ export default {
         invoiceDate: formatExportDate(form.date),
         item: form.itemType,
         price: +form.totalCost,
-        quantity: form.quantity
+        quantity: form.quantity,
+        description: form.itemDescription
       }
       showSuccessAlert.value = false;
       form.error = ''
@@ -310,6 +313,8 @@ export default {
             const RESPONSE = res.data.data
             if(RESPONSE.partnerCreditDeductionAdded){
               const { navigateTo } = utility('view-deductions'); navigateTo();
+            }else {
+              showMsg.value = "Error while sending distributor credit correction";
             }
           })
           .catch((error) => {
@@ -328,6 +333,7 @@ export default {
       quantityArray,
       showSuccessAlert,
       addDeductionsMethod,
+      showMsg
     }
   }
 }
